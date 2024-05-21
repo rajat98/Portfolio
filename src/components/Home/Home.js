fc6831014b3a7cb0a2419e18e38a21b2c1e36da6
Rajat Pawar <rajat.pawar100@gmail.com>
2024-05-20 23:06:45 -0700

    added contact sec

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import {AiFillGithub, AiFillInstagram} from "react-icons/ai";
+import {FaLinkedinIn} from "react-icons/fa";
 
 function Home() {
   return (
@@ -37,6 +39,56 @@ function Home() {
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
               />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>FIND ME ON</h1>
+              <p>
+                Feel free to <span className="purple">connect </span>with me
+              </p>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                      href="https://github.com/rajat98"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+                {/*<li className="social-icons">*/}
+                {/*  <a*/}
+                {/*    href="https://twitter.com/Soumyajit4419"*/}
+                {/*    target="_blank"*/}
+                {/*    rel="noreferrer"*/}
+                {/*    className="icon-colour  home-social-icons"*/}
+                {/*  >*/}
+                {/*    <AiOutlineTwitter />*/}
+                {/*  </a>*/}
+                {/*</li>*/}
+                <li className="social-icons">
+                  <a
+                      href="https://www.linkedin.com/in/rajatpawar/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                      href="https://www.instagram.com/rpaw_"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour home-social-icons"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+              </ul>
             </Col>
           </Row>
         </Container>
